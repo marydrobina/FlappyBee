1fd7bd86e4536dc382beeda0a63e549ef5ab194d
Kirill Sayed <sayed.kirill@gmail.com>
2021-09-27 16:36:42 +0300

    Created class Game and added __pycache__/ into .gitignore

--- game.py
+++ game.py
@@ -3,40 +3,13 @@ import random
 
 import pics
 
-pygame.init()
-
-display_width = 800
-display_height = 532
-
-display = pygame.display.set_mode((display_width, display_height))
-
-pygame.display.set_caption('Crazy Beeee')
-
-x = 10
-y = 532 - 70
-width = 70
-height = 70
-speed = 5
-
-is_jump = False
-jump_count = 11
-
-standL = False
-standR = False
-left = False
-right = False
-
-anim_count = 0
-lastMove = "right"
-
-clock = pygame.time.Clock()
-
-run = True
-bullets = []
-
 
 class Bird:
-    def __init__(self, away_y, speed, cd_hide):
+    def __init__(
+            self,
+            away_y: int,
+            speed: int,
+            cd_hide: int):
         self.x = random.randrange(10, 700)
         self.y = away_y
         self.ay = away_y
@@ -47,7 +20,7 @@ class Bird:
         self.come = True
         self.go_away = False
 
-    def draw(self):
+    def draw(self, display: pygame.Surface):
         if self.img_cnt == 4:
             self.img_cnt = 0
         if self.x > 350:
@@ -78,7 +51,13 @@ class Bird:
 
 
 class Honey:
-    def __init__(self, x, y, radius, color, facing):
+    def __init__(
+            self,
+            x: int,
+            y: int,
+            radius: int,
+            color: tuple,
+            facing: int):
         self.x = x
         self.y = y
         self.radius = radius
@@ -90,108 +69,143 @@ class Honey:
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 
-def usr_animation():
-    global anim_count
+class Game:
 
-    if anim_count + 1 >= 30:
-        anim_count = 0
+    def __init__(self, display_width: int, display_height: int):
+        self.display = pygame.display.set_mode((display_width, display_height))
+        self.coordinates = (10, display_height - 70)
+        self.size = {'width': 70, 'height': 70}
+        self.directions = {'left': False, 'right': False}
+        self.speed = 5
+        self.is_jump = False
+        self.jump_count = 11
+        self.standL = False
+        self.standR = False
+        self.anim_count = 0
+        self.last_move = "right"
+        self.clock = pygame.time.Clock()
+        self.run = True
+        self.bullets = list()
 
-    if left:
-        display.blit(pics.WALK_LEFT[anim_count // 6], (x, y))
-        anim_count += 4
-
-    elif right:
-        display.blit(pics.WALK_RIGHT[anim_count // 6], (x, y))
-        anim_count += 4
-
-    else:
-        if lastMove == "right":
-            display.blit(pics.BEE_STAND1, (x, y))
-
-        elif lastMove == "left":
-            display.blit(pics.BEE_STAND2, (x, y))
-
-
-def draw_window():
-    display.blit(pics.BG, (0, 0))
-    bird1.draw()
-    bird2.draw()
-    bird3.draw()
-
-    usr_animation()
-
-    for bullet in bullets:
-        bullet.draw(display)
-
-    pygame.display.update()
-
-
-def make_bullet(x, y, width):
-    for bullet in bullets:
-        if 800 > bullet.x > 0:
-            bullet.x += bullet.vel
+    def usr_animation(self):
+        if self.anim_count + 1 >= 30:
+            self.anim_count = 0
+        if self.directions['left']:
+            self.display.blit(pics.WALK_LEFT[self.anim_count // 6], self.coordinates)
+            self.anim_count += 4
+        elif self.directions['right']:
+            self.display.blit(pics.WALK_RIGHT[self.anim_count // 6], self.coordinates)
+            self.anim_count += 4
         else:
-            bullets.pop(bullets.index(bullet))
+            if self.last_move == "right":
+                self.display.blit(pics.BEE_STAND1, self.coordinates)
 
-    keys = pygame.key.get_pressed()
+            elif self.last_move == "left":
+                self.display.blit(pics.BEE_STAND2, self.coordinates)
 
-    if keys[pygame.K_f]:
-        if lastMove == "right":
-            facing = 1
-        else:
-            facing = -1
+    def draw_window(self, birds: tuple):
+        self.display.blit(pics.BG, (0, 0))
+        bird1, bird2, bird3 = birds
+        bird1.draw()
+        bird2.draw()
+        bird3.draw()
 
-        if len(bullets) < 10:
-            bullets.append(Honey(round(x + width // 2), round(y + width // 2),
-                                 6, (251, 236, 93), facing))
+        self.usr_animation()
 
+        for bullet in self.bullets:
+            bullet.draw(self.display)
 
-bird1 = Bird(-80, 5, 0)
-bird2 = Bird(-90, 6, 70)
-bird3 = Bird(-80, 5, 120)
+        pygame.display.update()
 
-while run:
-
-    clock.tick(100)
-    pygame.time.delay(22)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    keys = pygame.key.get_pressed()
-
-    make_bullet(x, y, width)
-
-    if keys[pygame.K_LEFT] and x > 5:
-        x -= speed
-        left = True
-        right = False
-        standL = True
-        standR = False
-        lastMove = "left"
-    elif keys[pygame.K_RIGHT] and x < 800 - width - 5:
-        x += speed
-        left = False
-        right = True
-        standR = True
-        standL = False
-        lastMove = "right"
-
-    if not is_jump:
-        if keys[pygame.K_SPACE]:
-            is_jump = True
-    else:
-        if jump_count >= - 11:
-            if jump_count < 0:
-                y += (jump_count ** 2) // 2
+    def make_bullet(self):
+        for bullet in self.bullets:
+            if 800 > bullet.x > 0:
+                bullet.x += bullet.vel
             else:
-                y -= (jump_count ** 2) // 2
-            jump_count -= 1
-        else:
-            is_jump = False
-            jump_count = 11
+                self.bullets.pop(self.bullets.index(bullet))
 
-    draw_window()
+        keys = pygame.key.get_pressed()
 
-pygame.quit()
+        if keys[pygame.K_f]:
+            if self.last_move == "right":
+                facing = 1
+            else:
+                facing = -1
+
+            if len(self.bullets) < 10:
+                x = self.coordinates[0]
+                y = self.coordinates[1]
+                self.bullets.append(
+                    Honey(
+                        round(x + self.size['width'] // 2),
+                        round(y + self.size['width'] // 2),
+                        6,
+                        (251, 236, 93),
+                        facing))
+
+    def run_game(self):
+        bird1 = Bird(-80, 5, 0)
+        bird2 = Bird(-90, 6, 70)
+        bird3 = Bird(-80, 5, 120)
+
+        while self.run:
+
+            self.clock.tick(100)
+            pygame.time.delay(22)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            keys = pygame.key.get_pressed()
+
+            self.make_bullet()
+            x = self.coordinates[0]
+
+            if keys[pygame.K_LEFT] and x > 5:
+                x -= self.speed
+                self.directions['left'] = True
+                self.directions['right'] = False
+                self.standL = True
+                self.standR = False
+                self.last_move = "left"
+            elif keys[pygame.K_RIGHT] and x < 800 - self.size['width'] - 5:
+                x += self.speed
+                self.directions['left'] = False
+                self.directions['right'] = True
+                self.standR = True
+                self.standL = False
+                self.last_move = "right"
+
+            if not self.is_jump:
+                if keys[pygame.K_SPACE]:
+                    self.is_jump = True
+            else:
+                y = self.coordinates[1]
+                if self.jump_count >= - 11:
+                    if self.jump_count < 0:
+                        y += (self.jump_count ** 2) // 2
+                    else:
+                        y -= (self.jump_count ** 2) // 2
+                    self.jump_count -= 1
+                else:
+                    self.is_jump = False
+                    self.jump_count = 11
+
+            self.draw_window((bird1, bird2, bird3))
+
+
+def main():
+    pygame.init()
+    pygame.display.set_caption('Crazy Beeee')
+    display_width = 800
+    display_height = 532
+    game = Game(
+        display_width=display_width,
+        display_height=display_height)
+    game.run_game()
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
