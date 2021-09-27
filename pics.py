0cd6d2e8d8cd6c81a984a5441ed23590f71c7dc2
Kirill Sayed <sayed.kirill@gmail.com>
2021-09-27 17:03:33 +0300

    Renamed directory with assets

--- pics.py
+++ pics.py
@@ -3,19 +3,19 @@
 
 import pygame
 
-WALK_RIGHT = [pygame.image.load('fpics/r1.png'), pygame.image.load('fpics/r2.png'),
-              pygame.image.load('fpics/r3.png'), pygame.image.load('fpics/r4.png'),
-              pygame.image.load('fpics/r5.png'), pygame.image.load('fpics/r6.png')]
-WALK_LEFT = [pygame.image.load('fpics/l1.png'), pygame.image.load('fpics/l2.png'),
-             pygame.image.load('fpics/l3.png'), pygame.image.load('fpics/l4.png'),
-             pygame.image.load('fpics/l5.png'), pygame.image.load('fpics/l6.png')]
+WALK_RIGHT = [pygame.image.load('assets/r1.png'), pygame.image.load('assets/r2.png'),
+              pygame.image.load('assets/r3.png'), pygame.image.load('assets/r4.png'),
+              pygame.image.load('assets/r5.png'), pygame.image.load('assets/r6.png')]
+WALK_LEFT = [pygame.image.load('assets/l1.png'), pygame.image.load('assets/l2.png'),
+             pygame.image.load('assets/l3.png'), pygame.image.load('assets/l4.png'),
+             pygame.image.load('assets/l5.png'), pygame.image.load('assets/l6.png')]
 
-BIRD_FLY_RIGHT = [pygame.image.load('fpics/f1r.png'), pygame.image.load('fpics/f2r.png')]
-BIRD_FLY_LEFT = [pygame.image.load('fpics/f1l.png'), pygame.image.load('fpics/f2l.png')]
+BIRD_FLY_RIGHT = [pygame.image.load('assets/f1r.png'), pygame.image.load('assets/f2r.png')]
+BIRD_FLY_LEFT = [pygame.image.load('assets/f1l.png'), pygame.image.load('assets/f2l.png')]
 
-BIRD_HIT_RIGHT = [pygame.image.load('fpics/gh1r.png'), pygame.image.load('fpics/gh2r.png')]
-BIRD_HIT_LEFT = [pygame.image.load('fpics/gh1l.png'), pygame.image.load('fpics/gh2l.png')]
+BIRD_HIT_RIGHT = [pygame.image.load('assets/gh1r.png'), pygame.image.load('assets/gh2r.png')]
+BIRD_HIT_LEFT = [pygame.image.load('assets/gh1l.png'), pygame.image.load('assets/gh2l.png')]
 
-BG = pygame.image.load('fpics/bg.jpg')
-BEE_STAND1 = pygame.image.load('fpics/r1.png')
-BEE_STAND2 = pygame.image.load('fpics/l1.png')
+BG = pygame.image.load('assets/bg.jpg')
+BEE_STAND1 = pygame.image.load('assets/r1.png')
+BEE_STAND2 = pygame.image.load('assets/l1.png')
